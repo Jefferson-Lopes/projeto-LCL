12a6d7d70d99b8ad220a893954225c633d6846d1
Jefferson-Lopes <jeff.777.lopes@gmail.com>
2020-12-04 09:45:57 -0300

    update

--- projeto-Quartus/memory.sv
+++ projeto-Quartus/memory.sv
@@ -20,38 +20,3 @@ module memory (we, address, data_in, data_out, debug);
 	end
 	
 endmodule
-
-/*
-module ram_single_port_ar_aw #(
-  parameter DATA_WIDTH = 8,
-  parameter ADDR_WITDH = 3
-)(
-  input                       we,    // write enable
-  input                       oe,    // output enable
-  input  [(ADDR_WITDH-1):0]   waddr, // write address
-  input  [(DATA_WIDTH-1):0]   wdata, // write data
-  input                       raddr, // read adddress
-  output [(DATA_WIDTH-1):0]   rdata  // read data
-);
-
-  reg [(DATA_WIDTH-1):0]      ram [0:2**ADDR_WITDH-1];
-  reg [(DATA_WIDTH-1):0]      data_out;
-
-  assign rdata = (oe && !we) ? data_out : {DATA_WIDTH{1'bz}};
-
-  always @*
-  begin : mem_write
-    if (we) begin
-      memory[address] = wdata;
-    end
-  end
-
-  always @* // if anything below changes (i.e. we, oe, raddr), execute this    
-  begin : mem_read
-    if (!we) begin
-      data_out = memory[address];
-    end
-  end
-
-endmodule
-*/
