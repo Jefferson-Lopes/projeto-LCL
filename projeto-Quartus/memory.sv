ddd211fb00d8c1280f419a2d9aafa9a4eee7d1db
Jefferson-Lopes <jeff.777.lopes@gmail.com>
2020-12-13 15:57:56 -0300

    update

--- projeto-Quartus/memory.sv
+++ projeto-Quartus/memory.sv
@@ -4,7 +4,7 @@ module memory (we, address, data_in, data_out);
 	input [7:0] data_in;
 	output [7:0] data_out;
 	
-	reg [7:0] memory [0:7];   //128 addresses with 8 bits each
+	reg [7:0] memory [0:7];   //256 addresses with 8 bits each
 
 	always @ * begin
 		if (we) begin          //write data
